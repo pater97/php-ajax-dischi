3de5ea8a10d4144975f75d6599809ac7399733e4
pater97 <giuseppepater997@gmail.com>
2021-12-09 15:57:28 +0100

    bug: chiamata api not found

---
+++ millestone2/asstets_js/app.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      dischi : []
+    },
+    mounted() {
+      axios
+        .get('./api/dischi.php')
+        .then(response => {
+          console.log(response);
+          this.dischi = response.data
+          console.log(this.dischi);
+        }).catch(error => {
+          console.log(error);
+        })
+    }
+  })
--- millestone2/index.php
+++ millestone2/index.php
@@ -1,33 +1,30 @@
 <?php
 require __DIR__ . '/layouts/head.php';
-require __DIR__ . '/api/dischi.php';
  ?>
 
-<main>
+<main id="app">
     <section class="main_container">
         <div class="row">
-            <?php   foreach($dischi as $disco)  : ?>
-                <div class="card_column">
+                <div class="card_column" v-for="disco in dischi" :key="dischi.id">
                     <!-- card -->
                     <div class="card">              
                         <!-- immgaine  -->
-                        <?php echo "<img src = '".$disco['poster']."'>"; ?>
+                        <img :src="disco.poster" alt="">
                         <!-- titolo -->
                         <h2>
-                            <?php echo $disco['title']; ?>
+                            {{disco.title}}
                         </h2>
                         <!-- artista -->
                         <h5>
-                            <?php echo $disco['author']; ?>
+                            {{disco.author}}
                         </h5>
                         <!-- anno d'uscita  -->
                         <h6>
-                            <?php echo $disco['year']; ?>
+                            {{disco.year}}
                         </h6>
                     </div>
                     <!-- /card -->
                 </div>
-            <?php endforeach ?>
         </div>
     </section>
 </main>
